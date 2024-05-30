94aac735bd1ee32cd832f37ef4fdda5610dd4965
Evan Drake <evanmdrake@icloud.com>
2024-05-29 20:07:46 -0700

    llvm initial blocks created
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib ( module Lib ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Functions
+
+data Operation = Add | Sub
+
+-- simple assignment type for llvm
+data Assignment = Bind (Name, Type, String) | Binary (Name, Type, Operation, String, String)
+
+instance Show Operation where
+  show Add      = "add "
+  show Sub      = "sub "
+
+instance Show Assignment where
+  show (Bind (n, t, v)) = ((show n) ++ " = " ++ (show t) ++ v ++ "\n")
+  show (Binary (n, t, o, s1, s2)) = ((show n) ++ " = " ++ show o ++ (show t) ++ s1 ++ ", " ++ s2 ++ "\n")
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,9 @@
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Lib
+import Functions
+
 main :: IO()
 main = defaultMain testing
 
@@ -8,5 +11,14 @@ testing :: TestTree
 testing = testGroup "All Tests"
   [
   testCase "smoke" $
-    True @?= True
+    True @?= True,
+  testCase "primitive Binding print" $
+    show (Bind (Local "x", Number, "5")) @?= "%x = i32 5\n",
+  testCase "primitive Addition print" $
+    show (Binary (Local "x", Number, Add, "x0", "5")) @?= "%x = add i32 x0, 5\n",
+  testCase "primitive Subtraction print" $
+    show (Binary (Local "x", Number, Sub, "x0", "1")) @?= "%x = sub i32 x0, 1\n",
+  testCase "function begin translation" $
+    let paramlist = Full [(Number, Global "x"), (Number, Local "y"), (Number, Local "z")] in
+    show (Primitive (Number, Global "sum", paramlist)) @?= "define i32 @sum(i32 @x, i32 %y, i32 %z) {"
   ]
